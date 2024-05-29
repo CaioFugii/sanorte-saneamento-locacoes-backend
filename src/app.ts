6f50879332153395afa17a309b3ff36190d3a6be
Caio Fugii <caio.fugii@hotmail.com>
2024-05-29 12:39:17 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+export default app;
