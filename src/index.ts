d3d043e4597d1b990a46a970786b9289ea52c793
Caio Fugii <caio.fugii@hotmail.com>
2024-06-16 16:32:33 -0300

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import { existsSync, mkdirSync } from "fs";
 import app from "./app";
 import { connectionPool } from "./repository/database-connection";
-import path from "path";
-import { startRoutine } from "./cron-routine/cron-job";
 
 const PORT = Number(process.env.PORT);
 
 const bootstrap = async () => {
   try {
     await connectionPool.connect();
-
-    startRoutine();
 
     app.listen(PORT, () => {
       console.log(`Server running on port: ${PORT}`);
