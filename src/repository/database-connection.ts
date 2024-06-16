0d98f06645a4e2ba1321c1cf0d47d57f6918c76a
Caio Fugii <caio.fugii@hotmail.com>
2024-06-16 19:40:38 -0300

    chore

--- src/repository/database-connection.ts
+++ src/repository/database-connection.ts
@@ -2,7 +2,7 @@ import { Client } from "pg";
 
 export const connectionPool = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 });
