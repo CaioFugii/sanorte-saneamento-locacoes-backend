c8dbe9293349b24e8193afa716570aebd68e5038
Caio Fugii <caio.fugii@hotmail.com>
2024-06-16 19:58:19 -0300

    chore

--- src/repository/database-connection.ts
+++ src/repository/database-connection.ts
@@ -1,8 +1,10 @@
 import { Client } from "pg";
 
 export const connectionPool = new Client({
-  connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  connectionString:
+    process.env.DATABASE_URL ??
+    "postgres://ucvrlbs2cretlg:p885330c9cb6a0c3fbaa094cca37d72ac0ecc4ace15384ddba0f3248950687e1f@ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9rvgaij88f60j",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
