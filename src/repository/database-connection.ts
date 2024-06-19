4ed839cc899b0afbb3284c067858dc2bba1dec2e
Caio Fugii <caio.fugii@hotmail.com>
2024-06-19 11:07:16 -0300

    chore

--- src/repository/database-connection.ts
+++ src/repository/database-connection.ts
@@ -3,7 +3,7 @@ import { Client } from "pg";
 export const connectionPool = new Client({
   connectionString:
     process.env.DATABASE_URL ?? "postgres://postgres:secret@localhost/postgres",
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
